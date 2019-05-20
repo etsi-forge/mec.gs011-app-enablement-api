2fcf465876d2bf3b70c85c16c807366d7649d47b
featherstone <walter.featherstone@viavisolutions.com>
2019-05-20 10:41:39 +0100

    grantTypes enum array. Updated jenkins script to use swagger-cli and speccy

--- .jenkins.sh
+++ .jenkins.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-specfiles=$(ls | egrep -i "^Mp1.(json|yaml)")
+specfiles=$(ls | egrep "^[^.]*.(json|yaml)")
 
 fres=0
 for i in $specfiles ; do
-	echo "-- Validating OpenAPI file $i..."
-	swagger-tools validate $i
-	res=$?
-	fres=$(($fres||$res))
-	echo -e "-- Validator returned $res.\n"
+    echo "-- Validating and linting OpenAPI file $i..."
+    swagger-cli validate "$i"
+    res=$?
+    speccy lint "$i"
+    res2=$?
+    fres=$(($fres||$res||$res2))
+    echo "--- Validator returned $res, linter returned $res2."
 done
 
-echo "-- Final validator returns $fres."
+echo "-- Final validation returns $fres."
 
 exit $fres
-
